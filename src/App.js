ca9560965361ad132ed13113052560743409bd00
sarraayari <96915255+sarraayari@users.noreply.github.com>
2023-05-16 14:45:57 +0200

    tt

--- src/App.js
+++ src/App.js
@@ -1,49 +1,85 @@
 
+// import React from 'react';
+// import 'react-bootstrap';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import './App.css';
+// import Footer from './Components/Footer';
+// import Teacher from './Components/Teachers';
+// import Student from './Components/Students';
+// import { BrowserRouter, Route , Routes} from "react-router-dom";
+// import Home from './Components/Home';
+// import AlertMessage from '../src/Components/AlertMessage';
+// import Login from './Components/Login';
+// import DB_teacher from './Components/DB_teacher';
+// import DB_student from './Components/DB_student';
+// import DB_contact from './Components/DB_contact';
+// function App() {
+ 
+//   return (
+//     <div className='App'>
+    
+    
+
+//       <BrowserRouter>
+      
+//       <Routes> 
+//       <Route exact path='/' element={<Home/>} ></Route> 
+//       <Route exact path='/Home' element={<Home/>} ></Route>  
+//        <Route exact path='/Student' element={<Student/>} ></Route>
+//        <Route exact path='/Teacher' element={<Teacher/>} ></Route>
+//        <Route exact path='/AlertMessage' element={<AlertMessage/>} ></Route>
+//        <Route exact path='/Login' element={<Login/>} ></Route>
+//        <Route exact path='/DB_student' element={<DB_student/>} ></Route>
+//        <Route exact path='/DB_contact' element={<DB_contact/>} ></Route>
+//        <Route exact path='/DB_teacher' element={<DB_teacher/>} ></Route>
+    
+//       </Routes>
+//       </BrowserRouter>  
+
+      
+//       <Footer/>
+//       </div>
+      
+      
+    
+//   );
+// }
+// export default App; 
+
+
 import React from 'react';
-import 'react-bootstrap';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Footer from './Components/Footer';
 import Teacher from './Components/Teachers';
 import Student from './Components/Students';
-import { BrowserRouter, Route , Routes} from "react-router-dom";
 import Home from './Components/Home';
-import AlertMessage from '../src/Components/AlertMessage';
+import AlertMessage from './Components/AlertMessage';
 import Login from './Components/Login';
 import DB_teacher from './Components/DB_teacher';
 import DB_student from './Components/DB_student';
 import DB_contact from './Components/DB_contact';
+
 function App() {
- 
   return (
     <div className='App'>
-    
-    
-
       <BrowserRouter>
-      
-      <Routes> 
-      <Route exact path='/' element={<Home/>} ></Route> 
-      <Route exact path='/Home' element={<Home/>} ></Route>  
-       <Route exact path='/Student' element={<Student/>} ></Route>
-       <Route exact path='/Teacher' element={<Teacher/>} ></Route>
-       <Route exact path='/AlertMessage' element={<AlertMessage/>} ></Route>
-       <Route exact path='/Login' element={<Login/>} ></Route>
-       <Route exact path='/DB_student' element={<DB_student/>} ></Route>
-       <Route exact path='/DB_contact' element={<DB_contact/>} ></Route>
-       <Route exact path='/DB_teacher' element={<DB_teacher/>} ></Route>
-    
-      </Routes>
-      </BrowserRouter>  
-
-      
-      <Footer/>
-      </div>
-      
-      
-    
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Home' element={<Home />} />
+          <Route path='/Student' element={<Student />} />
+          <Route path='/Teacher' element={<Teacher />} />
+          <Route path='/AlertMessage' element={<AlertMessage />} />
+          <Route path='/Login' element={<Login />} />
+          <Route path='/DB_student' element={<DB_student />} />
+          <Route path='/DB_contact' element={<DB_contact />} />
+          <Route path='/DB_teacher' element={<DB_teacher />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </div>
   );
 }
-export default App; 
 
-
+export default App;
